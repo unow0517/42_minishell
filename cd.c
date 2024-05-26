953c7a55ea2ad115172822c3d417e791e9c44afc
unow0517 <unow1992@gmail.com>
2024-05-26 17:50:40 +0200

    thalia'secho

--- cd.c
+++ cd.c
@@ -12,7 +12,6 @@
 
 #include "minishell.h"
 
-//FIND ENV WITH NAME
 t_env_mini	*env_search_name(char *name, t_env_mini *env_mini)
 {
 	if (!name || !env_mini)
@@ -31,34 +30,18 @@ t_env_mini	*env_search_name(char *name, t_env_mini *env_mini)
 
 void	run_cd(char *inpt, t_shell *shell_info)
 {
-	char	*path_input;
-	char	cwd[1024];
-	// char	*cwd_input;
+	char		*path_input;
+	char		cwd[1024];
 	t_env_mini	*env_mini;
 	t_env_mini	*env_mini_pwd;
 	t_env_mini	*env_mini_oldpwd;
-	// char	*slash_path;
-	// char	*path;
-  	// char *cwdptr;
-  	// cwdptr = ft_strdup(cwd);
+
   	getcwd(cwd, sizeof(cwd));
-	//   printf("cwd=%s\n", cwd);
   	env_mini_pwd = 0;
   	env_mini_oldpwd = 0;
   	env_mini_pwd = env_search_name("PWD", shell_info->env_mini);
   	env_mini_oldpwd = env_search_name("OLDPWD", shell_info->env_mini);
-
-	//OPTION 1. WHEN THE WHOLE INPUT IS PASSED
-  	path_input = inpt + 3;
-	//OPTION 1. END
-
-	// //OPTION 2. WHEN THE STRING AFTER 'cd ' IS PASSED
-  	// path_input = inpt + 3;
-	// //OPTION 2. END
-
-
-
-
+  	path_input = inpt;
 	if (chdir(path_input) == -1)
 	{
 		*(shell_info->status) = 1;
@@ -76,84 +59,9 @@ void	run_cd(char *inpt, t_shell *shell_info)
 	    	env_mini_oldpwd->value = shell_info->oldpwd;
 	  	else
 	  	{
-	    // printf("%s %s\n", shell_info->cwd, shell_info->oldpwd);
-	    env_mini = ft_lstnew_envmini("OLDPWD", shell_info->oldpwd);
-	    ft_lstlast_envmini(shell_info->env_mini)->next = env_mini;
+	    	env_mini = ft_lstnew_envmini("OLDPWD", shell_info->oldpwd);
+	    	ft_lstlast_envmini(shell_info->env_mini)->next = env_mini;
 	  	}
 		*(shell_info->status) = 0;
 	}
-
-
-	// if (access(path_input, R_OK) != -1)
-	// {
-
-	// 	cwd_input = ft_substr(path_input, 0, ft_strlen(cwd));
-	// 	if (ft_strncmp(cwd_input, cwd, ft_strlen(cwd_input)) == 0)
-	// 	{
-	// 		ft_memset(shell_info->oldpwd, 0, 1024);
-	// 		ft_strlcat(shell_info->oldpwd, shell_info->cwd, 1024);
-	// 		ft_memset(shell_info->cwd, 0, 1024);
-	// 		ft_strlcat(shell_info->cwd, path_input, 1024);
-	// 		// shell_info->cwd = ft_strdup(path_input);
-  //     // env_mini = ft_lstnew_envmini("OLDPWD", shell_info->cwd);
-	//     // ft_lstlast_envmini(shell_info->env_mini)->next = env_mini;
-  //     env_mini_pwd->value = shell_info->cwd;
-  //     if (env_mini_oldpwd)
-  //       env_mini_oldpwd->value = shell_info->oldpwd;
-  //     else
-  //     {
-  //       // printf("%s %s\n", shell_info->cwd, shell_info->oldpwd);
-  //       env_mini = ft_lstnew_envmini("OLDPWD", shell_info->oldpwd);
-	//       ft_lstlast_envmini(shell_info->env_mini)->next = env_mini;
-  //     }
-  //   }
-	// 	else
-	// 		ft_printf("minishell: cd : %s: Folder exist but absolute path is required\n", path_input);	
-	// }
-	// else
-	// 	ft_printf("minishell: cd : %s: Only absolute path is available\n", path_input);
-
-
-
-	// if (cwd)
-	// 	free(cwd);
 }
-
-		// printf("substr: %s\n", ft_substr(path_input, 0, ft_strlen(cwd)));
-		// printf("substrlen: %zu\n", ft_strlen(ft_substr(path_input, 0, ft_strlen(cwd))));
-		// printf("cwdlen: %zu\n", ft_strlen(cwd));
-		// printf("if %d\n", ft_substr(path_input, 0, ft_strlen(cwd)) == cwd);
-		// printf("cwd_input: %s\n", cwd_input);
-		// printf("cwd: %s\n", cwd);
-		// printf("cwd: %d\n", ft_strncmp(cwd_input, cwd, ft_strlen(cwd_input)));
-
-
-// //RELATIVE PATH
-// void	run_cd(char *inpt, t_mini *shell_info)
-// {
-// 	char	*rlt_path;
-// 	char	cwd[1024];
-// 	char	*slash_path;
-// 	char	*path;
-
-// 	// getcwd(cwd, sizeof(cwd));
-// 	// printf("cwd: %s\n", cwd);
-// 	rlt_path = inpt + 3;
-// 	slash_path = ft_strjoin("/", rlt_path);
-// 	if (access(ft_strjoin(cwd, slash_path), R_OK) != -1)
-// 	{
-// 		shell_info->cwd = ft_strjoin(cwd, slash_path);
-// 	}
-// 	else if (!(*rlt_path == '.' && *(rlt_path + 1) == '/')) //case of cd Nolibft
-// 		ft_printf("minishell: cd : %s: No such file or directory\n", rlt_path);
-// 	else if (*rlt_path == '.' && *(rlt_path + 1) == '.')
-	
-// 	else // case of cd ./Libft 
-// 	{
-// 		path = ft_strjoin(cwd, rlt_path + 1);
-// 		if (access(path, R_OK) != -1)
-// 			shell_info->cwd = path;
-// 		free(path);
-// 	}
-// 	free(slash_path);
-// }
--- echo.c
+++ echo.c
@@ -5,124 +5,81 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yowoo <yowoo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/19 16:56:43 by yowoo             #+#    #+#             */
-/*   Updated: 2024/04/19 16:57:29 by yowoo            ###   ########.fr       */
+/*   Created: 2024/05/26 17:49:45 by yowoo             #+#    #+#             */
+/*   Updated: 2024/05/26 17:49:45 by yowoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exp_from_beiginnig(char *inpt, t_shell *shell_info)
-{
-	char	q;
-	char	*first_q_ptr;
-	int		len;
-	int		noq_len;
-	char	*till_first_q;
-	char	*start;
-	char	*str_in_q;
+static void	echo_without_flag(char **split_arg);
+static void	echo_with_flag(char **split_arg);
 
-	q = 0;
-	len = 0;
-	noq_len = 0;
-	start = inpt;
-	while (inpt && *inpt)
-	{
-		if (*inpt == q)
-		{
-			str_in_q = ft_substr(first_q_ptr + 1, 0, len - 1);
-			shell_info->temp = ft_strjoin(shell_info->temp, str_in_q);
-			q = 0;
-		}
-		else if (*inpt == '\'' || *inpt == '"')
-		{
-			q = *inpt;
-			first_q_ptr = inpt;
-			till_first_q = ft_substr(start, 0, inpt - start);
-			// shell_info->temp = till_first_q;
-			shell_info->temp = ft_strjoin(shell_info->temp, str_in_q);
-		}
-		printf("len %d\n", len);
-		printf("q %c\n", q);
-		printf("inpt %s\n", inpt);
-		printf("shellinfo %s\n\n", shell_info->temp);
-		if (q)
-		{
-			len++;
-			noq_len = 0;
-		}
-		else
-		{
-			noq_len++;
-			len = 0;
-		}
-		inpt++;
-	}
-}
-
-// void	run_echo(char *inpt)
 void	run_echo(char *inpt, t_shell *shell_info)
 {
-	// char	*str;
-	// char	q;
+	char	**split_arg;
 
-	// (void)shell_info;
-	ft_printf("start: %s\n", inpt);
-	// str = inpt;
-	// q = first_quote(inpt);
-	// str = rm_quotes(str, q);
-	exp_from_beiginnig(inpt, shell_info);
-
-	// str = ec_rm_outest_q_and_exp(inpt, shell_info);
-	// ft_printf("test: %s\n", str);
-	ft_printf("RESULT: %s\n", shell_info->temp);
-
-	// write(2, "HELLO\n", ft_strlen("HELLO\n"));
-	//OPTION 1. WHEN THE WHOLE INPUT IS PASSED
-	// if (inputstartswith(inpt, "echo -n "))
-	// 	ft_printf("%s", inpt + 5);
-	// else
-	// 	ft_printf("%s\n", inpt + 5);
-	// *(shell_info->status) = 0;
-	//OPTION 1. END
-
-
-	// //OPTION 2. WHEN THE STRING AFTER 'echo ' IS PASSED
-	// if (inputstartswith(inpt, "echo -n "))
-	// 	ft_printf("%s", inpt);
-	// else
-	// 	ft_printf("%s\n", inpt);
-	// *(shell_info->status) = 0;
-	// // //OPTION 2. END
-
-
-	// char	**output;
-	// char	*str;
-
-	// output = ft_split(inpt + 5, ' ');
-	// // str = *output;
-	// // while (str)
-	// // {
-	// // 	str = backslash_piece(str);
-	// // 	str++;
-	// // }
-	// while (*output)
-	// {
-	// ft_printf("op %s\n",*output);
-	// // ft_printf("bs %s\n",backslash_piece(*output));
-	// 	// *output = backslash_piece(*output);
-	// 	output++;
-	// }
-	// str = *output;
-	// while (str)
-	// {
-	// 	ft_printf("%s", str);
-	// 	str++;
-	// }
-	// ft_printf("bs %s\n",backslash(inpt));
-	// ft_printf('\\');
+	split_arg = split_ms(inpt, ' ');
+	if (ft_strncmp("-n", split_arg[0], 2) == 0 && strlen(split_arg[0]) == 2)
+		echo_with_flag(split_arg);
+	else
+		echo_without_flag(split_arg);
+	*(shell_info->status) = 0;
 }
 
+static void	echo_without_flag(char **split_arg)
+{
+	int		i;
+	int		j;
+	int		sq;
+	int		dq;
 
+	i = 0;
+	sq = 0;
+	dq = 0;
+	while (split_arg &&split_arg[i])
+	{
+		j = 0;
+		if (i != 0)
+			write (1, " ", 1);
+		while (split_arg[i][j])
+		{
+			if ((split_arg[i][j] == '\'' && dq == 0) || \
+			(split_arg[i][j] == '"' && sq == 0))
+				update_quote_state_str(split_arg[i], &sq, &dq, j);
+			else
+				write (1, &(split_arg[i][j]), 1);
+			j++;
+		}
+		i++;
+	}
+	write (1, "\n", 1);
+}
 
-//ECHO WITH OPTION N!
+static void	echo_with_flag(char **split_arg)
+{
+	int		i;
+	int		j;
+	int		sq;
+	int		dq;
+
+	i = 1;
+	sq = 0;
+	dq = 0;
+	while (split_arg &&split_arg[i])
+	{
+		j = 0;
+		if (i != 1)
+			write (1, " ", 1);
+		while (split_arg[i][j])
+		{
+			if ((split_arg[i][j] == '\'' && dq == 0) || \
+			(split_arg[i][j] == '"' && sq == 0))
+				update_quote_state_str(split_arg[i], &sq, &dq, j);
+			else
+				write (1, &(split_arg[i][j]), 1);
+			j++;
+		}
+		i++;
+	}
+}
